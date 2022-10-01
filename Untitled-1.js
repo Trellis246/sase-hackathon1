54211b7f8cfe77d4f200c288cb4e0a6c4be378e6
Trellis246 <tokellis24@gmail.com>
2022-10-01 09:40:48 -0500

    modified:   Untitled-1.js 	new file:   sase-hackathon1/.gitignore 	new file:   sase-hackathon1/README.md 	new file:   sase-hackathon1/package-lock.json 	new file:   sase-hackathon1/package.json 	new file:   sase-hackathon1/public/favicon.ico 	new file:   sase-hackathon1/public/index.html 	new file:   sase-hackathon1/public/logo192.png 	new file:   sase-hackathon1/public/logo512.png 	new file:   sase-hackathon1/public/manifest.json 	new file:   sase-hackathon1/public/robots.txt 	new file:   sase-hackathon1/src/App.css 	new file:   sase-hackathon1/src/App.js 	new file:   sase-hackathon1/src/App.test.js 	new file:   sase-hackathon1/src/index.css 	new file:   sase-hackathon1/src/index.js 	new file:   sase-hackathon1/src/logo.svg 	new file:   sase-hackathon1/src/reportWebVitals.js 	new file:   sase-hackathon1/src/setupTests.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,14 +1,19 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>JavaScript in Body</h2>
-
-<p id="demo"></p>
-
-<script>
-document.getElementById("demo").innerHTML = "My First JavaScript";
-</script>
-
-</body>
-</html> 
+switch (new caseInput().getInput()){
+    case 1:
+        savings = savings - (savings * 0.3);
+        bills = bills - (bills * 0.3);
+        fun = fun - (fun * 0.4);
+        break;
+    case 2:
+        savings = savings - (savings * 0.4);
+        bills = bills - (bills * 0.4);
+        fun = fun - (fun * 0.2);
+        break;
+    case 3:
+        savings = savings - (savings * 0.3);
+        bills = bills - (bills * 0.5);
+        fun = fun - (fun * 0.2);
+        break;
+    default:
+        text: "Choose a case input";
+}
