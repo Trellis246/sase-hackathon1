8df109ccd3d9db95c672f4a291d2dfdca10b79c2
Trellis246 <tokellis24@gmail.com>
2022-10-01 10:01:33 -0500

    modified:   Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,19 +1,21 @@
-switch (new caseInput().getInput()){
-    case 1:
-        savings = savings - (savings * 0.3);
-        bills = bills - (bills * 0.3);
-        fun = fun - (fun * 0.4);
-        break;
-    case 2:
-        savings = savings - (savings * 0.4);
-        bills = bills - (bills * 0.4);
-        fun = fun - (fun * 0.2);
-        break;
-    case 3:
-        savings = savings - (savings * 0.3);
-        bills = bills - (bills * 0.5);
-        fun = fun - (fun * 0.2);
-        break;
-    default:
-        text: "Choose a case input";
+function main (){
+    switch (new caseInput().getInput()){
+        case 1:
+            savings = savings - (savings * 0.3);
+            bills = bills - (bills * 0.3);
+            fun = fun - (fun * 0.4);
+            break;
+        case 2:
+            savings = savings - (savings * 0.4);
+            bills = bills - (bills * 0.4);
+            fun = fun - (fun * 0.2);
+            break;
+        case 3:
+            savings = savings - (savings * 0.3);
+            bills = bills - (bills * 0.5);
+            fun = fun - (fun * 0.2);
+            break;
+        default:
+            text: "Choose a case input";
+    }
 }
